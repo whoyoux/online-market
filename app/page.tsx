902245c5cacfa597194362c031a4dcca13756eef
whoyoux <48690153+whoyoux@users.noreply.github.com>
2025-02-25 22:35:47 +0100

    added tailwind typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
 export default function Home() {
 	return (
-		<div>
-			<span>hello</span>
+		<div className="prose dark:prose-invert">
+			<h1>h1 test</h1>
+			<h2>h2 test</h2>
+			<h3>h3 test</h3>
+			<pre>code test</pre>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque odio
+				nisi animi accusantium molestiae? Deserunt rem consectetur voluptatibus
+				alias minima iure, amet ducimus nobis voluptatum repellat, molestiae
+				dolores, aperiam provident.
+			</p>
+			<span>Unordered list</span>
+			<ul>
+				<li>hello</li>
+				<li>hello</li>
+				<li>hello</li>
+				<li>hello</li>
+				<li>hello</li>
+			</ul>
+			<span>Ordered list</span>
+			<ol>
+				<li>hello</li>
+				<li>hello</li>
+				<li>hello</li>
+				<li>hello</li>
+				<li>hello</li>
+			</ol>
 		</div>
 	);
 }
