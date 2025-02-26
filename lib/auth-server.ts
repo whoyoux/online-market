8a75b6c6ede8f79749dbee73f03d125666a76f5a
whoyoux <48690153+whoyoux@users.noreply.github.com>
2025-02-26 23:42:52 +0100

    getSession wrapped in cache by react

--- lib/auth-server.ts
+++ lib/auth-server.ts
@@ -1,10 +1,22 @@
 import "server-only";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import { cache } from "react";
 
-export default async function getSession() {
-	const session = await auth.api.getSession({
-		headers: await headers(),
+// export default async function getSession() {
+// 	const session = await auth.api.getSession({
+// 		headers: await headers(),
+// 	});
+// 	return session;
+// }
+
+const getSession = async () =>
+	cache(async () => {
+		console.log("[DEDUP] GetSession")
+		const session = await auth.api.getSession({
+			headers: await headers(),
+		});
+		return session;
 	});
-	return session;
-}
+
+export default getSession;
